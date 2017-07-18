7f63c329165eeddd8efeae0119203a0ef68a1466
mirabitl <acqilc@localhost.localdomain>
2017-07-18 16:26:01 +0200

    add analysis

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@
 rsync -axv /opt/zdaq/lib/ $1:/opt/dhcal/lib/
 rsync -axv /opt/lydaq/drivers/lib/ $1:/opt/dhcal/lib/
 rsync -axv /opt/lydaq/apps/lib/ $1:/opt/dhcal/lib/
+rsync -axv /opt/lydaq/analysis/lib/ $1:/opt/dhcal/lib/
+rsync -axv /opt/lydaq/analysis/bin/ $1:/opt/dhcal/bin/
 rsync -axv /opt/lydaq/apps/bin/ $1:/opt/dhcal/bin
 rsync -axv /usr/local/lib/libzmq.so* $1:/opt/dhcal/lib/
