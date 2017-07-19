9cdf72f5138fa32f2c909b212877751f953ff70d
laurent.mirabito <mirabito@cern.ch>
2017-07-19 11:52:37 +0000

    deploy zdaq/etc

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 rsync -axv /opt/zdaq/lib/ $1:/opt/dhcal/lib/
+rsync -axv /opt/zdaq/bin/ $1:/opt/dhcal/bin/
+rsync -axv /opt/zdaq/etc/ $1:/opt/dhcal/etc
 rsync -axv /opt/lydaq/drivers/lib/ $1:/opt/dhcal/lib/
 rsync -axv /opt/lydaq/apps/lib/ $1:/opt/dhcal/lib/
 rsync -axv /opt/lydaq/analysis/lib/ $1:/opt/dhcal/lib/
