92ff9bf455dde4fc7afc3155f2352320375fe080
Laurent <laurent.mirabito@cern.ch>
2017-08-22 15:38:43 +0200

    Genesys server

---
+++ apps/Genesys/include/LGenesysServer.hh
@@ -0,0 +1,51 @@
+#ifndef _LGenesysServer_h
+
+#define _LGenesysServer_h
+#include <iostream>
+
+#include <string.h>
+#include<stdio.h>
+#include "baseApplication.hh"
+
+#include "Genesys.hh"
+
+using namespace std;
+#include <sstream>
+#include "ReadoutLogger.hh"
+#include <zmq.hpp>
+
+namespace lydaq
+{
+  class LGenesysServer : public zdaq::baseApplication
+  {
+  public:
+    LGenesysServer(std::string name);
+    // Transition
+    void open(zdaq::fsmmessage* m);
+    void close(zdaq::fsmmessage* m);
+    void start(zdaq::fsmmessage* m);
+    void monitor();
+    void stop(zdaq::fsmmessage* m);
+    // Access to the interface
+    lydaq::Genesys* getLVGenesysInterface(){  //std::cout<<" get Ptr "<<_lv<<std::endl;
+      return _lv;}
+    // Status
+    Json::Value status();
+    // Commande
+    void c_status(Mongoose::Request &request, Mongoose::JsonResponse &response);
+    void c_on(Mongoose::Request &request, Mongoose::JsonResponse &response);
+    void c_off(Mongoose::Request &request, Mongoose::JsonResponse &response);
+  private:
+    //zdaq::fsm* _fsm;
+    zdaq::fsmweb* _fsm;
+ 
+    lydaq::Genesys* _lv;
+    boost::thread_group g_store;
+    bool _running;
+    uint32_t _period;
+    zmq::context_t* _context;
+    zmq::socket_t *_publisher;
+  };
+};
+#endif
+
---
+++ apps/Genesys/src/LGenesysServer.cxx
@@ -0,0 +1,218 @@
+
+#include "LGenesysServer.hh"
+using namespace zdaq;
+using namespace lydaq;
+
+void lydaq::LGenesysServer::open(zdaq::fsmmessage* m)
+{
+  LOG4CXX_INFO(_logLdaq," CMD: "<<m->command());
+
+  std::string device;
+  if (m->content().isMember("device"))
+    { 
+      device=m->content()["device"].asString();
+      this->parameters()["device"]=m->content()["device"];
+    }
+  else
+    device=this->parameters()["device"].asString();
+
+  uint32_t port;
+  if (m->content().isMember("port"))
+  { 
+    port=m->content()["port"].asUInt();
+    this->parameters()["port"]=m->content()["port"];
+  }
+  else
+    port=this->parameters()["port"].asUInt();
+  std::cout<<"calling open "<<std::endl;
+  if (_lv!=NULL)
+    delete _lv;
+  
+  
+  
+  _lv= new lydaq::Genesys(device,port);
+ 
+
+  
+}
+void lydaq::LGenesysServer::close(zdaq::fsmmessage* m)
+{
+  LOG4CXX_INFO(_logLdaq," CMD: "<<m->command());
+  if (_lv==NULL)
+    {
+       LOG4CXX_ERROR(_logLdaq,"No ZUPInterface opened");
+       return;
+    }
+
+  delete _lv;
+  _lv=NULL;
+}
+void lydaq::LGenesysServer::stop(zdaq::fsmmessage* m)
+{
+  LOG4CXX_INFO(_logLdaq," CMD: "<<m->command());
+  if (_lv==NULL)
+    {
+      LOG4CXX_ERROR(_logLdaq,"No ZUPInterface opened");
+       return;
+    }
+    _running=false;
+  g_store.join_all();
+}
+void lydaq::LGenesysServer::start(zdaq::fsmmessage* m)
+{
+  LOG4CXX_INFO(_logLdaq," CMD: "<<m->command());
+  if (_lv==NULL)
+    {
+      LOG4CXX_ERROR(_logLdaq,"No ZUPInterface opened");
+       return;
+    }
+  if (m->content().isMember("period"))
+    { 
+      this->parameters()["period"]=m->content()["period"];
+    }
+  if (!this->parameters().isMember("period"))
+    {
+      LOG4CXX_ERROR(_logLdaq,"Please define Reading period");
+      return;
+    }
+  else
+    _period=this->parameters()["period"].asUInt();
+  
+  
+  g_store.create_thread(boost::bind(&lydaq::LGenesysServer::monitor, this));
+  _running=true;
+    
+}
+
+void lydaq::LGenesysServer::monitor()
+{
+  Json::FastWriter fastWriter;
+  while (_running)
+  {
+    
+    if (_publisher==NULL)
+    {
+      LOG4CXX_ERROR(_logLdaq,"No publisher defined");
+      continue;
+    }
+    std::stringstream sheader;
+    sheader<<"ZUPLV";
+    zmq::message_t ma1((void*)sheader.str().c_str(), sheader.str().length(), NULL); 
+    _publisher->send(ma1, ZMQ_SNDMORE); 
+    Json::Value jstatus=this->status();
+    std::string scont= fastWriter.write(jstatus);
+    zmq::message_t ma2((void*)scont.c_str(), scont.length(), NULL); 
+    _publisher->send(ma2);
+    if (!_running) break;
+    ::sleep(_period);
+  }
+}
+Json::Value lydaq::LGenesysServer::status()
+{
+  Json::Value r=Json::Value::null;
+  r["name"]="ZUP";
+  r["status"]=Json::Value::null;
+   if (_lv==NULL)
+    {
+      LOG4CXX_ERROR(_logLdaq,"No ZUPInterface opened");
+       return r;
+    }
+    float vset=_lv->ReadVoltageSet();
+   float vout=_lv->ReadVoltageUsed();
+   r["vset"]=vset;
+   r["vout"]=vout;
+   r["iout"]=_lv->ReadCurrentUsed();
+   if (abs(1-abs(vset-vout)/vset)<0.8)
+     r["status"]="OFF";
+   else
+     r["status"]="ON";
+   return r;
+}
+
+
+
+void lydaq::LGenesysServer::c_status(Mongoose::Request &request, Mongoose::JsonResponse &response)
+{
+  if (_lv==NULL)
+    {
+      LOG4CXX_ERROR(_logLdaq,"No ZUPInterface opened");
+       response["STATUS"]=Json::Value::null;
+       return;
+    }
+ 
+  response["STATUS"]=this->status();
+}
+void lydaq::LGenesysServer::c_on(Mongoose::Request &request, Mongoose::JsonResponse &response)
+{
+  if (_lv==NULL)
+  {
+    LOG4CXX_ERROR(_logLdaq,"No ZUPInterface opened");
+    response["STATUS"]=Json::Value::null;
+    return;
+  }
+ 
+  _lv->ON();
+  ::sleep(2);
+  response["STATUS"]=this->status();
+}
+void lydaq::LGenesysServer::c_off(Mongoose::Request &request, Mongoose::JsonResponse &response)
+{
+  if (_lv==NULL)
+  {
+    LOG4CXX_ERROR(_logLdaq,"No ZUPInterface opened");
+    response["STATUS"]=Json::Value::null;
+    return;
+  }
+ 
+  _lv->OFF();
+  ::sleep(2);
+  response["STATUS"]=this->status();
+}
+
+
+
+lydaq::LGenesysServer::LGenesysServer(std::string name) : zdaq::baseApplication(name),_lv(NULL),_running(false),_context(NULL),_publisher(NULL),_period(120)
+{
+
+  
+ 
+
+  //_fsm=new zdaq::fsm(name);
+  _fsm=this->fsm();
+
+  
+// Register state
+  _fsm->addState("STOPPED");
+  _fsm->addState("RUNNING");
+
+  _fsm->addTransition("OPEN","CREATED","STOPPED",boost::bind(&lydaq::LGenesysServer::open, this,_1));
+  _fsm->addTransition("START","STOPPED","RUNNING",boost::bind(&lydaq::LGenesysServer::start, this,_1));
+  _fsm->addTransition("STOP","RUNNING","STOPPED",boost::bind(&lydaq::LGenesysServer::stop, this,_1));
+  _fsm->addTransition("CLOSE","STOPPED","CREATED",boost::bind(&lydaq::LGenesysServer::close, this,_1));
+  
+ _fsm->addCommand("STATUS",boost::bind(&lydaq::LGenesysServer::c_status,this,_1,_2));
+ _fsm->addCommand("ON",boost::bind(&lydaq::LGenesysServer::c_on,this,_1,_2));
+ _fsm->addCommand("OFF",boost::bind(&lydaq::LGenesysServer::c_off,this,_1,_2));
+ 
+
+ 
+  
+
+  char* wp=getenv("WEBPORT");
+  if (wp!=NULL)
+    {
+      std::cout<<"Service "<<name<<" started on port "<<atoi(wp)<<std::endl;
+    _fsm->start(atoi(wp));
+    }
+
+    if (this->parameters().isMember("serverName"))
+    {
+      _context= new zmq::context_t(1);
+      _publisher= new zmq::socket_t((*_context), ZMQ_PUB);
+      _publisher->bind(this->parameters()["serverName"].asString());
+      
+    }
+}
+
+
+
