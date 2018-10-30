f1496bb2cac6f0025383c5d678c453dfbeabba16
acqilc <acqilc@localhost.localdomain>
2018-10-30 11:43:59 +0100

    Injector board

---
+++ drivers/FebInj/include/FebInj.hh
@@ -0,0 +1,96 @@
+#ifndef _FebInj_H_
+#define _FebInj_H_
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+#include <math.h>
+#include <dirent.h>
+
+#include <wiringPi.h>
+#include <wiringPiSPI.h>
+
+
+/**
+x"00" => w_enableHigh0_7_reg 
+x"01" => w_enableHigh8_15_reg
+x"02" => w_enableHigh16_23_reg
+default = x"FFFFFF" -> tout actif
+x"03" => w_enableLow0_7_reg
+x"04" => w_enableLow8_15_reg
+x"05" => w_enableLow16_23_reg
+default =x"000000" -> tout inactif
+x"06" => w_TriggerSource_reg
+default = x"02"
+x"01" -> interne single
+x"02" -> interne multiple
+x"04" -> externe single 
+x"08" ->  externe multiple
+x"10" -> software 
+x"07" => w_SoftTrigger_reg
+default =x"00"
+x"01" -> bit de trigger
+x"08" => w_TriggerNumber0_7_reg
+x"09" => w_TriggerNumber8_15_reg
+x"0A" => w_TriggerNumber16_23_reg
+x"0B" => w_TriggerNumber24_31_reg
+default= x"000010"
+x"0C" => w_TriggerDelay0_7_reg
+x"0D" => w_TriggerDelay8_15_reg
+x"0E" => w_TriggerDelay16_23_reg
+x"0F" => w_TriggerDelay24_31_reg
+default =x"000000"
+x"10" => w_TriggerDuration0_7_reg
+x"11" => w_TriggerDuration8_15_reg
+x"12" => w_TriggerDuration16_23_reg
+x"13" => w_TriggerDuration24_31_reg
+default = x"0000FF"
+x"14" => w_mcp4021_value_reg
+default = x"00"
+bit 0-6 : value 
+bit 7 : 1: enable loading
+          0 : do nothing
+x"15" => w_software_veto_reg
+default = x"00"
+bit 0: 0 : no pulse for external trigger  
+         1 : external trigger enabled
+
+ */
+
+
+namespace lydaq {
+  
+  class FebInj
+  {
+  public:
+    FebInj();
+    ~FebInj();
+    
+    void myAnalogWrite (struct wiringPiNodeStruct *node, int addr, int value);
+    int myAnalogRead (struct wiringPiNodeStruct *node, int addr);
+    void setMask(uint32_t mask,uint32_t hr);
+    void setTriggerSource(uint32_t source);
+    void softwareTrigger();
+    void pauseExternalTrigger();
+    void resumeExternalTrigger();
+    void setNumberOfTrigger(uint32_t n);
+    void setDelay(uint32_t n);
+    void setDuration(uint32_t n);
+    void setPulseHeight(uint32_t n);
+  private:
+    struct wiringPiNodeStruct *node ;
+	
+    unsigned char spiData [2] ;
+
+  };
+};
+#endif
---
+++ drivers/FebInj/src/FebInj.cxx
@@ -0,0 +1,119 @@
+#include "FebInj.hh"
+using namespace lydaq;
+
+lydaq::FebInj::FebInj()
+{
+  if (wiringPiSPISetup (0, 10000) < 0)	// 10MHz Max
+    return -1 ;
+
+  node = wiringPiNewNode (64, 2) ;
+
+  node->fd          = 0 ;
+  node->analogWrite = myAnalogWrite ;
+  node->analogRead = myAnalogRead ;
+
+}
+ 
+lydaq::FebInj::~FebInj()
+{
+}
+
+
+
+void lydaq::FebInj::myAnalogWrite (struct wiringPiNodeStruct *node, int addr, int value)
+{
+
+
+
+  spiData [0] = (addr&0x7f) ;
+  spiData [1] = value&0xff ;
+
+  wiringPiSPIDataRW (node->fd, spiData, 2) ;
+}
+
+int lydaq::FebInj::myAnalogRead (struct wiringPiNodeStruct *node, int addr)
+{
+  unsigned char spiData [2] ;
+
+
+  spiData [0] = (addr&0x7f) +0x80;
+  spiData [1] = 0 ;
+
+  wiringPiSPIDataRW (node->fd, spiData, 2) ;
+  return spiData [1];
+}
+
+void lydaq::FebInj::setMask(uint32_t mask,uint32_t hr)
+{
+
+  uint8_t c[3];
+  for (int i=0;i<3)
+    {
+      c[i]=(mask>>(i*8))&0xFF;
+      if (!hr)
+	myAnalogWrite(node,i,c[i]);
+      else
+	myAnalogWrite(node,i+3,c[i]);
+    }
+}
+void lydaq::FebInj::setTriggerSource(uint32_t source)
+{
+  myAnalogWrite(node,6,source &0xFF);
+}
+void lydaq::FebInj::softwareTrigger()
+{
+  myAnalogWrite(node,7,1);
+  usleep(100);
+  myAnalogWrite(node,7,0);
+}
+void lydaq::FebInj::pauseExternalTrigger()
+{
+  myAnalogWrite(node,0x15,0);
+}
+void lydaq::FebInj::resumeExternalTrigger()
+{
+  myAnalogWrite(node,0x15,1);
+}
+
+void lydaq::FebInj::setNumberOfTrigger(uint32_t n)
+{
+
+  uint8_t c[4];
+  for (int i=0;i<4)
+    {
+      c[i]=(n>>(i*8))&0xFF;
+      myAnalogWrite(node,i+0x8,c[i]);
+    }
+  
+}
+void lydaq::FebInj::setDelay(uint32_t n)
+{
+
+  uint8_t c[4];
+  for (int i=0;i<4)
+    {
+      c[i]=(n>>(i*8))&0xFF;
+      myAnalogWrite(node,i+0xc,c[i]);
+    }
+  
+}
+void lydaq::FebInj::setDuration(uint32_t n)
+{
+
+  uint8_t c[4];
+  for (int i=0;i<4)
+    {
+      c[i]=(n>>(i*8))&0xFF;
+      myAnalogWrite(node,i+0x10,c[i]);
+    }
+  
+}
+void lydaq::FebInj::setPulseHeight(uint32_t n)
+{
+
+  uint8_t c=n&0x7f+128;
+  myAnalogWrite(node,0x14,c[i]);
+    }
+  
+}
+
