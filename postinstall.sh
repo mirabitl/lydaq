5142a63994e315431a9989e27298d993ad3e086b
Lagarde François <lagarde@ipnl.in2p3.fr>
2019-12-16 16:32:52 +0100

    add zdaq python scripts in installation

--- postinstall.sh
+++ postinstall.sh
@@ -2,6 +2,7 @@
 cd /opt/lydaq
 mkdir -p /opt/dhcal/script
 find . -name '*.py' -exec ln -sf /opt/lydaq/{} /opt/dhcal/script/ \;
+find /opt/zdaq -name '*.py' -exec ln -sf /opt/zdaq/{} /opt/dhcal/script/ \;
 echo "export PYTHONPATH=/opt/dhcal/script/:${PYTHONPATH}" >> ~/.bashrc
 echo "export PYTHONSTARTUP=/opt/lydaq/etc/.pythonrc" >> ~/.bashrc
 touch ${HOME}/.python_history
