1d58b2ffd7e2f1afa2702ae3fd44f2c180dfc003
mirabitl <pi@raspberrypi>
2020-01-23 14:40:49 +0100

    Hierachical SConstruct

--- drivers/hih8000/include/hih8000.hh
+++ drivers/hih8000/include/hih8000.hh
@@ -17,9 +17,12 @@
 #include <dirent.h>
 extern "C" {
 #include <linux/i2c-dev.h>
+#include <linux/types.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
 }
 
-//#include <i2c/smbus.h>
+#include <i2c/smbus.h>
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 
