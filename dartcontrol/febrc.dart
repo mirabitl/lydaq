52d544ac6b5d80b724487ed39bdb9552cf4b072a
mirabito <mirabito@ipnl.in2p3.fr>
2020-06-04 16:25:43 +0200

    FEB support

---
+++ dartcontrol/febrc.dart
@@ -0,0 +1,225 @@
+import 'daqcontrol.dart';
+//import 'rcbase.dart';
+import 'dart:convert';
+
+class febRC extends daqControl {
+  /// Constructor
+  febRC(String account, String config) : super(account, config) {}
+
+  /// Connection
+  Future<void> Connect() async {
+    await this.parseMongo();
+    await this.discover();
+  }
+
+  /// Commands
+  ///
+  ///  Builder Status
+  ///
+  Future<String> BuilderStatus() async {
+    Map rep = new Map();
+    for (var k in appMap.entries) {
+      if (k.key != "BUILDER") continue;
+      for (var s in k.value) {
+        Map r = new Map();
+        r["run"] = -1;
+        r["event"] = -1;
+        r["url"] = s.host_name;
+
+        var mr = json.decode(await s.sendCommand("STATUS", new Map()));
+
+        if (mr['status'] != "FAILED") {
+          r["run"] = mr["answer"]["answer"]["run"];
+          r["event"] = mr["answer"]["answer"]["event"];
+          r["builder"] = mr["answer"]["answer"]["difs"];
+          r["built"] = mr["answer"]["answer"]["build"];
+          r["total"] = mr["answer"]["answer"]["total"];
+          r["compressed"] = mr["answer"]["answer"]["compressed"];
+          r["time"] = DateTime.now().millisecondsSinceEpoch / 1000;
+        }
+        rep["${s.host_name}_${s.infos['instance']}"] = r;
+      }
+    }
+    return json.encode(rep);
+  }
+
+  /// TDCSERVER Status
+  Future<String> SourceStatus(String name) async {
+    Map rep = new Map();
+    for (var k in appMap.entries) {
+      if (k.key != name) continue;
+      for (var s in k.value) {
+        var mr = json.decode(await s.sendCommand("STATUS", new Map()));
+        //print(mr);
+        if (mr['status'] == "FAILED") {
+          rep["${s.host_name}_${s.infos['instance']}"] = mr;
+        } else
+          rep["${s.host_name}_${s.infos['instance']}"] =
+              mr["answer"]["TDCSTATUS"];
+      }
+    }
+    return json.encode(rep);
+  }
+
+  /// Trigger Status
+  Future<String> TriggerStatus(String name) async {
+    Map rep = new Map();
+    for (var k in appMap.entries) {
+      if (k.key != name) continue;
+      for (var s in k.value) {
+        var mr = json.decode(await s.sendCommand("STATUS", new Map()));
+        //print(mr);
+        if (mr['status'] == "FAILED") {
+          rep["${s.host_name}_${s.infos['instance']}"] = mr;
+        } else
+          rep["${s.host_name}_${s.infos['instance']}"] = mr["answer"];
+      }
+    }
+    return json.encode(rep);
+  }
+
+  /// FEB specific
+  /// Set6BDac
+  ///
+  Future<String> set6BDac(int dac) async {
+    Map param = new Map();
+    param["value"] = dac;
+    return await processCommand("SET6BDAC", "TDCSERVER", param);
+  }
+
+  ///cal6BDac
+  ///
+  Future<String> cal6BDac(String mask, int shift) async {
+    Map param = new Map();
+    param["shift"] = shift;
+    final number = int.parse(mask, radix: 16);
+    param["mask"] = number;
+    return await processCommand("CAL6BDAC", "TDCSERVER", param);
+  }
+
+  /// DAQ
+  ///
+  /// Initialise
+  ///
+  Future<String> daq_initialise() async {
+    Map r = new Map();
+
+    /// Initilise MDCC
+    ///
+    var s = json.decode(
+        await appMap['MDCCSERVER'][0].sendTransition("OPEN", new Map()));
+    r["MDCCSERVER"] = s;
+
+    /// Configure Builder
+    for (var x in appMap['BUILDER']) {
+      var s = json.decode(await x.sendTransition("CONFIGURE", new Map()));
+      r["BUILDER_${x.appInstance}"] = s;
+    }
+
+    /// Initialise TDCSERVER
+    for (var x in appMap['TDCSERVER']) {
+      var s = json.decode(await x.sendTransition("INITIALISE", new Map()));
+      r["TDCSERVER_${x.appInstance}"] = s;
+    }
+
+    return json.encode(r);
+  }
+
+  ///
+  /// Configure
+  ///
+  Future<String> daq_configure() async {
+    Map r = new Map();
+
+    /// Configure TDCSERVER
+    for (var x in appMap['TDCSERVER']) {
+      var s = json.decode(await x.sendTransition("CONFIGURE", new Map()));
+      r["TDCSERVER_${x.appInstance}"] = s;
+    }
+
+    return json.encode(r);
+  }
+
+  ///
+  /// Stop
+  ///
+  Future<String> daq_stop() async {
+    Map r = new Map();
+
+    /// Stop trigger
+    var s = json.decode(
+        await appMap['MDCCSERVER'][0].sendTransition("PAUSE", new Map()));
+    r["TRIGGER"] = s;
+
+    /// Stop TDCSERVER
+    for (var x in appMap['TDCSERVER']) {
+      s = json.decode(await x.sendTransition("STOP", new Map()));
+      r["TDCSERVER_${x.appInstance}"] = s;
+    }
+
+    /// Stop Builder
+    for (var x in appMap['BUILDER']) {
+      s = json.decode(await x.sendTransition("STOP", new Map()));
+      r["BUILDER_${x.appInstance}"] = s;
+    }
+    return json.encode(r);
+  }
+
+  ///
+  /// DESTROY
+  ///
+  Future<String> daq_destroy() async {
+    Map r = new Map();
+
+    /// Stop TDCSERVER
+    for (var x in appMap['TDCSERVER']) {
+      var s = json.decode(await x.sendTransition("DESTROY", new Map()));
+      r["TDCSERVER_${x.appInstance}"] = s;
+    }
+
+    return json.encode(r);
+  }
+
+  /// Start
+  Future<String> daq_start(int run,
+      {String location = "UNKNOWN", String comment = "Not set"}) async {
+    /// Run from DB
+    var nrun = run;
+    if (nrun == 0) {
+      await mongoAccess.open();
+      //await m.listConfigurations();
+      nrun = await mongoAccess.getRun(location, comment);
+      await mongoAccess.close();
+    } else
+      nrun = run;
+
+    ///
+    Map r = new Map();
+
+    /// Stop Builder
+    for (var x in appMap['BUILDER']) {
+      Map param = new Map();
+      param['run'] = nrun;
+      var s = json.decode(await x.sendTransition("START", param));
+      r["BUILDER_${x.appInstance}"] = s;
+      print(s);
+    }
+
+    /// Start TDCSERVER
+    for (var x in appMap['TDCSERVER']) {
+      var s = json.decode(await x.sendTransition("START", new Map()));
+      r["TDCSERVER_${x.appInstance}"] = s;
+      print(s);
+    }
+
+    /// Start trigger
+    var s = json.decode(
+        await appMap['MDCCSERVER'][0].sendTransition("RESET", new Map()));
+    s = json.decode(
+        await appMap['MDCCSERVER'][0].sendTransition("ECALRESUME", new Map()));
+    r["TRIGGER"] = s;
+    print(s);
+
+    return json.encode(r);
+  }
+}
